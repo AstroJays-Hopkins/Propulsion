29c18f866a16f203f17c0d37062af50a1a19a59e
Andrew Colombo <acolomb2@jhu.edu>
2018-02-22 18:47:21 -0500

    tweaked output generator

--- param_output.m
+++ param_output.m
@@ -1,8 +1,8 @@
 load('motor_param_output.mat');
 
-fid = fopen('motor_params.txt', 'w');
+fid = fopen('BJ-01_motor_params.txt', 'w');
 
-fprintf(fid, 'MOTOR PARAMETERS CALCULATED BY thrust_curve.m\nUpdated Feb 22, 2018\n\n\n\n');
+fprintf(fid, 'ASTROJAYS\nBJ-01 MOTOR PARAMETERS CALCULATED BY thrust_curve.m\nUpdated Feb 22, 2018\n\n\n\n');
 
 fprintf(fid, 'GENERAL MOTOR PERFORMANCE\n');
 fprintf(fid,'**************************************************************\n');
@@ -21,7 +21,7 @@ fprintf(fid, 'Pressure (Pa): %f\tTemperature (K): %f\nVelocity (m/s): %f\n******
 fprintf(fid, 'ENGINE DESIGN PARAMETERS\n');
 fprintf(fid, '**************************************************************\n');
 fprintf(fid, 'Propellant Mass (kg): %f\nOxizider Mass (kg): %f\nEstimated Ox Mass Flow Rate (kg/s): %f\n', m_fuel, m_ox, mdot_ox);
-fprintf(fid, 'Initial Inner Port Radius (m): %f\nOuter Propellant Diameter (m): %f\nLength of Propellant Grain (m): %f\n\n', rin_fuel, rout_fuel, L_fuel);
+fprintf(fid, 'Initial Inner Port Radius (m): %f\nOuter Propellant Radius (m): %f\nLength of Propellant Grain (m): %f\n\n', rin_fuel, rout_fuel, L_fuel);
 fprintf(fid, 'Expansion Ratio: %f\nInlet Diameter (m): %f\nThroat Diameter (m): %f\nExit Diameter (m): %f\n', ER, sqrt(4*D_star^2), D_star, D_e);
 fprintf(fid, '**************************************************************\n\n\n\nEND OUTPUT');
 fclose(fid);
