cead01255c392aebe59e63c96c7b712ce796e437
Dan Zanko <dzank97@gmail.com>
2019-09-04 13:47:40 -0400

    updated N2O prop lookup functions

---
+++ Test Data and Analysis/CdA_Extraction.m
@@ -0,0 +1,8 @@
+%% Documentation
+% Cd*A Extraction for Cold Flow Tests 1 & 2
+% Original Author: Dan Zanko (09/04/2019)
+
+%% Setup
+% loading test data files
+load('ColdFlow1.mat')
+load('ColdFlow2.mat')
