6991b619c0b823b22b0a883a7b424ed58d1b1c70
Dan Zanko <dzank97@gmail.com>
2019-09-12 12:47:19 -0400

    Changed name of old sim, created new perf script

---
+++ bj01_motor_perf/scripts/BJ01_BurnSim.m
@@ -0,0 +1,17 @@
+%% Documentation
+% Engine burn simulation script
+% BJ-01 Hybrid Engine Development - HDPE + N2O
+% Written by Dan Zanko - 09/12/2019
+%
+%
+% Structure of script:
+%0. Unit conversion constants
+%1. Initialization of constants and vectors
+
+%% 0. Unit Conversions
+PSItoPa = 6894.74; %[psi to Pa]
+LBFtoN = 4.44822; %[lbf to N]
+MtoIN = 39.3701; %[m to in]
+KGtoLBM = 2.20462; %[kg to lbm]
+
+%% 1. Initialization 
