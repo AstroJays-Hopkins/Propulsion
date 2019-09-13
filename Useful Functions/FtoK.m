3657a8b5dac99b11388e4968a41c476b02c1cba4
Dan Zanko <dzank97@gmail.com>
2019-09-13 16:56:53 -0400

    Organizing

---
+++ Useful Functions/FtoK.m
@@ -0,0 +1,7 @@
+function [temp_K] = FtoK(temp_F)
+% Converts an input temperature from °F to Kelvin
+
+temp_K = ((temp_F-32)*(9/5))+273.15;
+
+end
+
--- bj01_motor_perf/scripts/BJ01_BurnSim.m
+++ bj01_motor_perf/scripts/BJ01_BurnSim.m
@@ -2,16 +2,86 @@
 % Engine burn simulation script
 % BJ-01 Hybrid Engine Development - HDPE + N2O
 % Written by Dan Zanko - 09/12/2019
-%
-%
-% Structure of script:
-%0. Unit conversion constants
-%1. Initialization of constants and vectors
+% Last Updated: 09/12/2019
+
+% ----------------------
+% STRUCTURE OF SCRIPT:
+% 0. Unit conversion constants
+% 1. Inputting Engine Geometry (at start of burn)
+% 2. Initial Conditions of Burn
+
+% ----------------
+% NOMENCLATURE:
+% - "RT" = "Run Tank", the flight oxidizer tank
+% - "liq" = refers to the bulk liquid phase of N2O in the RT
+% - "ull" = refers to the "ullage" in the RT
+% - "CC" = "Combustion Chamber", the 
+
+
+% -------------
+% NOTES:
+% - Solid fuel configuration is that of a single, axially aligned
+%   cylindrical port
+% - Nozzle is conical
 
 %% 0. Unit Conversions
-PSItoPa = 6894.74; %[psi to Pa]
-LBFtoN = 4.44822; %[lbf to N]
-MtoIN = 39.3701; %[m to in]
-KGtoLBM = 2.20462; %[kg to lbm]
+Conv.PSItoPa = 6894.74; %[psi to Pa]
+Conv.LBFtoN = 4.44822; %[lbf to N]
+Conv.MtoFT = 3.28084; %[m to ft]
+Conv.MtoIN = 39.3701; %[m to in]
+Conv.KGtoLBM = 2.20462; %[kg to lbm]
 
-%% 1. Initialization 
+%% 1. Inputting Engine Geometry (NON-variable system parameters)
+
+% ------ Loss Coefficients ------ %
+% All of these are "K" values for the component/config
+loss.teeStraight = ; % loss coeff assoc. with flow through a tee going straight through
+loss.tee90 = ; % loss coeff assoc. with flow through a bent line, changing dir by 90°
+loss.bend = ; % loss coeff assoc. with flow through a bent line, changing dir by 90°
+loss.mNPTtoTube = ; % loss coeff
+loss.NOSolenoid = ; % loss coeff of N.O. solenoid used in sys
+loss.NCSolenoid = ; % loss coeff of N.C. solenoid used in sys {NOS Super Pro Shot}
+
+
+% ------ Vent Line Geometry & Flow/Loss Coefficients ------ %
+VentLine.length1 = /Conv.MtoFT; % length of first segment of vent line (the vertical portion) [ft --> m]
+VentLine.length2 = /Conv.MtoFT; % length of 2nd segment of vent line (the horizontal portion) [ft --> m]
+VentLine.length3 = /Conv.MtoFT; % length of outlet to amb segment of vent line (the horizontal portion) [ft --> m]
+
+% ------ Run Tank Geometry ------ %
+RT.bulk.vol = /(Conv.MtoFt^3); % total internal volume of N2O flight tank [ft^3 --> m^3]
+RT.bulk.height = /Conv.MtoFT; % total height of internal volume of RT [ft --> m]
+
+% ------ Injection Line Geometry & Flow/Loss Coefficients ------ %
+InjLine.length1 = /Conv.MtoFT; % length of first segment of vent line (the vertical portion) [ft --> m]
+InjLine.length2 = /Conv.MtoFT; % length of 2nd segment of vent line (the horizontal portion) [ft --> m]
+InjLine.length3 = /Conv.MtoFT; % length of outlet to amb segment of vent line (the horizontal portion) [ft --> m]
+
+% ------ Injector Geometry & Flow Coefficients ------ %
+Injector.CdA = /(Conv.MtoFt^2); % effective discharge area of injector [ft^2 --> m^2]
+
+% ------ Combustion Chamber Geometry ------ %
+Fuel.OuterR = /Conv.MtoIN; % outer radius of the fuel [in --> m]
+
+% ------ Nozzle Geometry ------ %
+Nozzle.DivAngle = deg2rad(15); % half angle of divergence of conical nozzle [rad]
+
+%% 2. Initial Conditions (& those that are engineer variable)
+
+% ------ Ambient Init Conditions ------ %
+Amb.alt = /Conv.MtoFT; % altitude above sea-level at start of burn [ft --> m]
+
+% ------ Run Tank Init Conditions ------ %
+RT.bulk.mass = /Conv.KGtoLBM; % initial total mass of N2O in RT (both liquid and ullage) [lbm --> kg]
+RT.liq.temperature = ; % initial temperature of bulk N2O liquid [°F]
+
+% ------ Fuel Init Conditions ------ %
+Fuel.InnerR = /conv.MtoIN; % initial inner radius of solid fuel port [in --> m]
+
+% ------ Nozzle Init Conditions ------ %
+Nozzle.throat.dia = ; % initial throat diameter of nozzle [ft]
+Nozzle.exit.dia = ; % initial exit diameter of nozzle [ft]
+
+
+
+
