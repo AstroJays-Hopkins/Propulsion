03715e09803fc6ec0b4483bf4a310662876af4a0
Dan Zanko <dzank97@gmail.com>
2019-09-13 16:59:05 -0400

    Organizing

--- bj01_motor_perf/functions/densitylookup_n2o_SI.m
+++ bj01_motor_perf/functions/densitylookup_n2o_SI.m
@@ -1,4 +1,8 @@
 function [density] = densitylookup_n2o_SI(p)  %density of saturated nitrous oxide liquid
+
+%%!!THIS IS LEGACY SOFTWARE (as of 09/13/2019) USEFUL ONLY FOR ORIGINAL MOTOR
+%%SIZING SCRIPT. HENCEFORTH USE refpropm.m or N2Olookup.m for
+%%thermophysical properties of N2O!!!
 rho = [907.4, 881.6, 853.5, 822.2, 786.6, 743.9, 688, 589.4, 452]; % [kg/m3]
 press = [3127, 3547, 4007, 4510, 5060, 5660, 6315, 7033, 7251]*1000; % [Pa]
 density = interp1(press, rho, p);
